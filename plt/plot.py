7d359e021440d796abd2d287f312743fa1d419fc
volmodaoist <1304548501@qq.com>
2024-04-10 11:59:04 +0800

    增加了热力图绘制工具

--- plt/plot.py
+++ plt/plot.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 FMTS = (
         'r-s',   # 红色实线方形标记
@@ -76,3 +77,39 @@ def plot_figure(X, Y = None, xlabel = None, ylabel = None, legend = None,
     plt.savefig(save_name)
 
 
+
+'''
+param {*} matrix        热力图矩阵
+param {*} xlabel        横轴坐标
+param {*} ylabel        纵轴坐标
+param {*} title         热力图矩阵标题
+param {*} cmap          颜色主题，通常我们使用 magma 主题
+                            1. 'coolwarm': 冷暖色调交替的主题，适合表示正负值的差异。
+                            2. 'magma': 黑紫红色调的主题，具有较高的对比度和可读性。
+                            3. 'inferno': 黑橙红色调的主题，也具有较高的对比度。
+                            4. 'plasma': 蓝紫橙色调的主题，颜色变化较为均匀。
+param {*} save_path     图像存储路径
+'''
+def plot_heatmap(matrix, xlabel = '', ylabel = '', title = 'Heatmap', cmap = 'viridis', save_path = 'heatmap.png'):    
+    plt.figure(figsize=(10, 8))
+
+    # 使用seaborn绘制热力图
+    sns.set_theme(font_scale = 1.8)
+    ax = sns.heatmap(matrix, annot=True, fmt=".2f", cmap=cmap, linewidths=.5,
+                     cbar_kws={'label': 'Similarity'}, square=True)
+    # 设置坐标轴标签
+    ax.set_xlabel(xlabel, fontsize=16, fontname="sans-serif")
+    ax.set_ylabel(ylabel, fontsize=16, fontname="sans-serif")
+    
+    # 设置颜色条标签字体大小
+    cbar = ax.collections[0].colorbar
+    cbar.ax.tick_params(labelsize=14)
+    
+    # 设置坐标轴刻度字体
+    plt.xticks(fontsize=14, fontname="sans-serif")
+    plt.yticks(fontsize=14, fontname="sans-serif")
+    
+    # 显示图形
+    plt.title(title, fontsize=18, fontname="sans-serif")
+    plt.tight_layout()
+    plt.savefig(save_path)
