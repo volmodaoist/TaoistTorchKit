5553899b3e4303f1ae13b5ad593f3bf1419f9726
volmodaoist <1304548501@qq.com>
2024-04-10 12:07:33 +0800

    补充了一些工具函数的说明

--- ops/LayerOps.py
+++ ops/LayerOps.py
@@ -2,6 +2,19 @@
 import torch.nn as nn
 
 
+'''
+    移除模块 remove_module
+        案例:  remove_bn 主要用于差分隐私
+    
+    替换模块 replace_module
+        案例:  replace_bn2gn 主要用于差分隐私
+            
+    增加模块 append_module2layer
+    
+    其它辅助函数:
+        1. get_in_channels
+        2. get_out_channelss
+'''
 
 def remove_module(module: nn.Module, target_module_type: type) -> nn.Module:
     '''递归移除指定类型的模块，将其换成恒等映射，target_module_type 标记要被替换的模块
@@ -28,28 +41,27 @@ def replace_moduleA2B(module: nn.Module, source_module_type: type, target_module
 
 
 
-def remove_bn(module: nn.Module) -> nn.Module:
-    ''' 递归移除BN模块，将其换成恒等映射，主要作用在于验证BN模块带来的性能提升，
-        或是为了令模型适配差分隐私训练模式，当然 opacus 自带的  ModuleValidator.fix 更好!
+def get_in_channels(module):
+    ''' 递归地获取模块的输入通道数
     '''
-    return remove_module(module, (nn.BatchNorm1d, nn.BatchNorm2d, nn.BatchNorm3d))
-
-
-
-def replace_bn2gn(module: nn.Module, num_groups: int = 32) -> nn.Module:
-    ''' 递归替换所有 BN 模块将其变成 GN 模块，在差分隐私的上下文中，
-        使用标准的批量归一化需要使用所有样本的统计信息，可能会泄露个体数据点的信息，
-        因而通常会使用 Group Normalization 作为代替品，其对同层样本通道进行分组，分别计算每组的均值和方差。
-    '''
+    # 直接返回模块的 in_channels 属性
+    if hasattr(module, 'in_channels'):
+        return module.in_channels
     
-    def _create_gn(bn_module):
-        """从BN模块创建GN模块，保持通道数相同。"""
-        num_channels = bn_module.num_features
-        return nn.GroupNorm(num_groups=num_groups, num_channels=num_channels)
-    
-    return replace_moduleA2B(module, (nn.BatchNorm1d, nn.BatchNorm2d, nn.BatchNorm3d), _create_gn)
-
-
+    # 若是 nn.Sequential 或 nn.ModuleList，递归地检查第一个模块
+    elif isinstance(module, (nn.Sequential, nn.ModuleList)):
+        for submodule in module:
+            in_channels = get_in_channels(submodule)
+            if in_channels is not None:
+                return in_channels
+            
+    # 若是 nn.Module 模块，检查这个模块里面第一个包含in_channels属性的模块
+    elif isinstance(module, nn.Module):
+        for _, submodule in list(module.named_children()):
+            in_channels = get_in_channels(submodule)
+            if in_channels is not None:
+                return in_channels                
+    return None
 
 def get_out_channels(module):
     ''' 递归地获取模块的输出通道数。
@@ -92,4 +104,35 @@ def append_module2layer(model, target_modules, attachment):
             append_module2layer(module, target_modules, attachment)
     return model
 
+
+
+
+
+
+
+
+
+
+
+
+def remove_bn(module: nn.Module) -> nn.Module:
+    ''' 递归移除BN模块，将其换成恒等映射，主要作用在于验证BN模块带来的性能提升，
+        或是为了令模型适配差分隐私训练模式，当然 opacus 自带的  ModuleValidator.fix 更好!
+    '''
+    return remove_module(module, (nn.BatchNorm1d, nn.BatchNorm2d, nn.BatchNorm3d))
+
+
+
+def replace_bn2gn(module: nn.Module, num_groups: int = 32) -> nn.Module:
+    ''' 递归替换所有 BN 模块将其变成 GN 模块，在差分隐私的上下文中，
+        使用标准的批量归一化需要使用所有样本的统计信息，可能会泄露个体数据点的信息，
+        因而通常会使用 Group Normalization 作为代替品，其对同层样本通道进行分组，分别计算每组的均值和方差。
+    '''
+    
+    def _create_gn(bn_module):
+        """从BN模块创建GN模块，保持通道数相同。"""
+        num_channels = bn_module.num_features
+        return nn.GroupNorm(num_groups=num_groups, num_channels=num_channels)
+    
+    return replace_moduleA2B(module, (nn.BatchNorm1d, nn.BatchNorm2d, nn.BatchNorm3d), _create_gn)
 
