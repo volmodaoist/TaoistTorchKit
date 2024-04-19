9fbde7d23c6148a8ab0b3d5d86ca8a0954ab5479
volmodaoist <1304548501@qq.com>
2024-04-19 23:22:45 +0800

    开坑对抗攻击

---
+++ attack/__init__.py
@@ -0,0 +1,10 @@
+'''
+TODO # 封装 cleverhans/foolbox
+
+ 1. 对抗攻击领域必备网站，列举了当前各种模型性能的排行榜/文章:
+    https://robustbench.github.io/#leaderboard
+ 
+ 2. 对抗攻击当前通常使用 AutoAttack 作为 benchmark:
+    https://github.com/fra31/auto-attack
+
+'''
